b87acbe04d9da7eb6ce8fea73fa1f448027690b9
muhammadteguhhernawankurnia <moch.teguhhernawankurnia@gmail.com>
2023-01-08 17:57:27 +0700

    login register with bycript done

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,27 +1,48 @@
 const authModel = require('../model/auth.model');
+const bcrypt = require('bcrypt');
 
 const authController = {
-  login: (req, res) =>
-    authModel
+  login: (req, res) => {
+    return authModel
       .login(req.body)
-      .then((result) =>
-        res.status(200).send({
+      .then((result) => {
+        return res.status(200).send({
           message: 'success',
           data: {
             user: result,
-            token: 'adnajdjNjnjXJnd/,',
+            token: '*Xkjkdfndk',
           },
-        })
-      )
-      .catch((error) => res.status(500).send({ message: error })),
+        });
+      })
+      .catch((error) => {
+        return res.status(500).send({ message: error });
+      });
+  },
 
-  register: (req, res) =>
-    authModel
-      .register(req.body)
-      .then((result) =>
-        res.status(200).send({ message: 'success', data: result })
-      )
-      .catch((error) => res.status(500).send({ message: error })),
+  register: (req, res) => {
+    // if(req.body.password == 0){
+
+    // } validasi eksplorasi pr bikin validasi kalo password kurang dari 6 karakter dll
+    console.log(req.body.password);
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+      if (err) {
+        return res.status(500).send({ message: err.message });
+      } else {
+        const request = {
+          username: req.body.username,
+          password: hash,
+        };
+        return authModel
+          .register(request)
+          .then((result) => {
+            return res.status(200).send({ message: 'success', data: result });
+          })
+          .catch((error) => {
+            return res.status(500).send({ message: error });
+          });
+      }
+    });
+  },
 };
 
 module.exports = authController;
--- src/model/auth.model.js
+++ src/model/auth.model.js
@@ -1,23 +1,31 @@
 const db = require('../../helper/connection');
 const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
 
 const authModel = {
   login: ({ username, password }) => {
+    console.log(username, password);
     return new Promise((resolve, reject) => {
       db.query(
-        `SELECT * FROM users WHERE username=$1 AND password=$2`,
-        [username, password],
+        `SELECT * FROM users WHERE username = $1`,
+        [username],
         (err, result) => {
-          if (err) {
-            return reject(err.message);
-          } else {
-            if (result.rows.length == 0) {
-              //username salah
-              return reject('username/password salah');
-            } else {
-              return resolve(result.rows[0]);
+          //username = unique || email =  unique
+          if (err) return reject(err.message);
+          if (result.rows.length == 0) return reject('username/password salah'); //ketika username salah
+
+          bcrypt.compare(
+            password,
+            result.rows[0].password,
+            function (err, hashingResult) {
+              //parameter dari user itu ada yang tidak valid (kosong)
+              //dari database
+              //bycript ada error yang kita tidak tahu
+              if (err) return reject('username/password salah'); //ketika kesalahan hashing(bycrpt)
+              if (!hashingResult) return reject('username/password salah'); //ketika password salah
+              return resolve('berhasil login');
             }
-          }
+          );
         }
       );
     });
